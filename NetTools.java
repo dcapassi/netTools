6046f80dda1892aaad537a461e8b17735325f975
Diego Capassi <diego.capassi.moreira@gmail.com>
2021-04-27 13:38:42 -0300

    wip

--- NetTools.java
+++ NetTools.java
@@ -5,9 +5,9 @@ public class NetTools {
 	public static boolean verbose = false;
 	
 	public static boolean isIpv4(String ipAddress) {
-				
+		
 		if (verbose) {
-			System.out.println("Checking if the string: "+ ipAddress +" has the IPv4 format.");
+			System.out.println("Checking if the string: "+ ipAddress +" has a valid IPv4 format.");
 		}
 		
 		String[] ipAddressOctets = ipAddress.split("\\.");
@@ -17,7 +17,7 @@ public class NetTools {
 			System.out.println("Number of Octects: "+ numberOfOctects);
 		}
 		
-		//Check if the string has four octects
+		//	Check if the string has four octects
 		if (numberOfOctects != 4) {
 			if (verbose) {
 				System.out.println("IPv4 check failed: Does not have four octects");
@@ -25,8 +25,8 @@ public class NetTools {
 			return false;
 		}
 		
-		//Convert the octect to Int
-		//Check if it is the range between 0 and 255
+		//	Convert the octect to Int
+		//	Check if it is the range between 0 and 255
 		
 		int number = 0;
 		int count = 0;
@@ -43,7 +43,6 @@ public class NetTools {
 				}
 				return false;     
 	        }finally {
-				System.out.println(number);
 				count++;
 				if (number<0 | number>255) {
 					if (verbose) {
@@ -54,5 +53,110 @@ public class NetTools {
 	        }
 		}
 		return true;
+	} // End of the isIpv4 method
+	
+	public static boolean isIpv6(String ipAddress) {
+		return true;
 	}
+	
+	public static boolean isAValidMask(String mask) {
+
+		//	Check if the Mask has a valid Ipv4 format
+		//	4 Octects
+		//	Numbers from 0 to 255
+		
+		boolean isIpv4 = isIpv4(mask);
+		if (!isIpv4) {
+			return false;
+		}
+		
+		String maskBinary32bits = binary32bits(mask);
+		boolean transition = false;
+		
+		for (int i = 0; i < maskBinary32bits.length()-1;i++) {
+			if (maskBinary32bits.charAt(i) != maskBinary32bits.charAt(i+1)) {
+				if (transition == true) {
+					System.out.println("Invalid Mask!");
+					return false;
+				}else {
+					transition = true;
+				}
+			}
+		}
+
+		return true;
+
+		}
+	
+	public static String binaryByteWithLeadingZeroes(int decimalOctect) {
+		
+		if (decimalOctect < 0 | decimalOctect > 255 ) {
+			return null;
+		}
+		
+		String binaryString = Integer.toBinaryString(decimalOctect);
+		String leadingZeros = "";
+		
+		int count = 8 - binaryString.length();
+		
+		while (count > 0) {
+			leadingZeros = leadingZeros + "0";
+			count--;
+		}
+		
+		binaryString = leadingZeros  + binaryString;
+		
+		return binaryString;
+		
+	}
+	
+	public static String binary32bits(String fourOctectsDecimal) {
+	String[] decimalOctects = fourOctectsDecimal.split("\\.");
+	String binary32bits = "";
+	for (int i = 0; i < 4; i++) {
+		binary32bits = binary32bits + 
+		binaryByteWithLeadingZeroes(Integer.parseInt(decimalOctects[i]));
+	}
+	return binary32bits;
 }
+	public static String binaryAnd(String binary32bits1, String binary32bits2) {
+		//check if both entries are 32 bits long
+		if (binary32bits1.length()!=32 | binary32bits2.length()!=32 ) {
+			return null;
+		}
+		String and = "";
+		int digit1, digit2;
+		for (int i = 0; i < 32; i++) {
+			digit1 = (Character.getNumericValue(binary32bits1.charAt(i)));
+			digit2 = (Character.getNumericValue(binary32bits2.charAt(i)));
+			and = and + Integer.toString(digit1&digit2);
+		}
+		return and;
+	}
+	
+	public static String binary32bitsFormated(String binary32bits, boolean formatDecimal) {
+		if (binary32bits.length()!=32) {
+			return null;
+		}
+		int count = 1;
+		String octect = "";
+		String format = "";
+		String ipFormated = "";
+		for (int i = 0; i < 32; i++) {
+			octect = octect + binary32bits.charAt(i);
+			if (count == 8) {
+				if (formatDecimal) {
+					format = Integer.toString((Integer.parseInt(octect,2)));
+				}else {
+					format = octect;
+				}
+				ipFormated = ipFormated + "." + format;
+				octect = "";
+				count = 1;
+			}else {
+			count++;
+			}
+		}
+		return ipFormated.substring(1);
+	}
+} 
--- Run.java
+++ Run.java
@@ -4,12 +4,25 @@ public class Run {
 
 	public static void main(String[] args) {
 		
+		String ipAddress;
 		NetTools.verbose = true;
-		String ipAddress = "192.168.1.1";
+		ipAddress = "192.168.1.1";
 		System.out.println(NetTools.isIpv4(ipAddress));
-		
-		
 
+		
+		String mask;
+		NetTools.verbose = true;
+		mask = "255.255.255.192";
+		System.out.println(NetTools.isAValidMask(mask));
+		
+		String binary1 = NetTools.binary32bits("172.16.1.193");
+		String binary2 = NetTools.binary32bits("255.255.255.224");
+		System.out.println(binary1);
+		System.out.println(binary2);
+		String result = NetTools.binary32bitsFormated(NetTools.binaryAnd(binary1, binary2),true);
+		
+		System.out.println(result);
+		
 	}
 
 }
